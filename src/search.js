9522061765f7714b1d04fdf166bac129bb6925b7
Ryan McGeary <ryan@mcgeary.org>
2018-01-05 19:57:50 -0700

    Refactor by removing duplicated logic

--- src/search.js
+++ src/search.js
@@ -3,64 +3,49 @@
 const emojilib = require('emojilib')
 const emojiNames = emojilib.ordered
 
-module.exports = function search (searchTerm) {
-  if (!searchTerm) {
-    const results = []
-    emojiNames.forEach((name) => {
-      const emoji = emojilib.lib[name].char
-      if (!emoji) return
-      results.push({
-        title: emoji,
-        subtitle: `Copy "${emoji}" (${name}) to clipboard`,
-        arg: emoji,
-        icon: { path: `./icons/${name}.png` }
-      })
-    })
-    return {items: results}
-  }
-
-  let ret = {items: []}
-
-  // search for the term within the names
-  const _searchTerm = searchTerm
-    .replace(/:/g, '') // :unamused: => unamused
-    .replace(/\s/g, '') // 'thumbs up' => 'thumbsup'
-  const nameResults = emojiNames.filter((name) => name === _searchTerm || name.includes(_searchTerm))
-  if (nameResults.length > 0) {
-    const results = []
-    nameResults.forEach((name) => {
-      const emoji = emojilib.lib[name].char
-      if (!emoji) return
-      results.push({
-        title: name,
-        subtitle: `Copy "${emoji}" (${name}) to clipboard`,
-        arg: emoji,
-        icon: { path: `./icons/${name}.png` }
-      })
-    })
-    ret.items = ret.items.concat(results)
-  }
-
-  // search for the term within the aliases
-  const aliasResults = []
-  for (var i = 0; i < emojiNames.length; i += 1) {
-    const obj = emojilib.lib[emojiNames[i]]
-    obj._name = emojiNames[i]
-    const filtered = obj.keywords.filter((keyword) => keyword.includes(_searchTerm))
-    if (filtered.length > 0) aliasResults.push(obj)
-  }
+const formattedResults = (names) => {
   const results = []
-  aliasResults.forEach((obj) => {
-    const emoji = obj.char
+  names.forEach((name) => {
+    const emoji = emojilib.lib[name].char
     if (!emoji) return
     results.push({
-      title: obj._name,
-      subtitle: `Copy "${emoji}" (${obj._name}) to clipboard`,
+      title: name,
+      subtitle: `Copy "${emoji}" (${name}) to clipboard`,
       arg: emoji,
-      icon: { path: `./icons/${obj._name}.png` }
+      icon: { path: `./icons/${name}.png` }
     })
   })
-  ret.items = ret.items.concat(results)
+  return results
+}
+
+const all = () => formattedResults(emojiNames)
+
+// search within the names
+const matchingName = (searchTerm) => {
+  const names = emojiNames.filter((name) => name.includes(searchTerm))
+  return formattedResults(names)
+}
+
+// search within the aliases
+const matchingAlias = (searchTerm) => {
+  const names = []
+  emojiNames.forEach((name) => {
+    if (emojilib.lib[name].keywords.some((keyword) => keyword.includes(searchTerm))) {
+      names.push(name)
+    }
+  })
+  return formattedResults(names)
+}
+
+module.exports = function search (searchTerm) {
+  if (!searchTerm) return { items: all() }
+
+  const _searchTerm = searchTerm.replace(/[:\s]/g, '') // :thumbs up: => thumbsup
+
+  const ret = { items: [] }
+
+  ret.items = ret.items.concat(matchingName(_searchTerm))
+  ret.items = ret.items.concat(matchingAlias(_searchTerm))
 
   const uniq = new Set(ret.items)
   ret.items = Array.from(uniq)
